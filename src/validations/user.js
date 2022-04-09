f6cf82b0e8218e69276f5047e974e7ab3730cf16
Vishal Kumar <vishal194kumar@gmail.com>
2022-04-09 16:35:36 +0530

    Removed obsolete code

--- src/validations/user.js
+++ src/validations/user.js
@@ -14,34 +14,6 @@ const loginSchema = {
 	}),
 };
 
-const createUserSchema = {
-	body: Joi.object().keys({
-		firstName: Joi.string().trim().required().label(`First Name`),
-		lastName: Joi.string().trim().optional().allow(``).label(`Last Name`),
-		email: Joi.string().trim().required().email().label(`Email`),
-		password: Joi.string().trim().required().label(`Password`),
-		reviewees: Joi.array()
-			.items(Joi.string().trim())
-			.min(0)
-			.optional()
-			.label(`Reviewee IDs`),
-		isAdmin: Joi.boolean().default(false),
-	}),
-};
-
-const assignReviewersSchema = {
-	body: Joi.object().keys({
-		userId: Joi.string().trim().required().label(`User ID`),
-		reviewerId: Joi.array()
-			.items(Joi.string().trim())
-			.min(1)
-			.required()
-			.label(`Reviewer IDs`),
-	}),
-};
-
 module.exports = {
 	loginSchema,
-	createUserSchema,
-	assignReviewersSchema,
 };
